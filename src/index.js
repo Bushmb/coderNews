8ee249dfd923f41979e54bfc410e277ecfd32d06
bushmb <bushmb@gmail.com>
2017-03-30 22:32:24 -0600

    first commmit - full working auth

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// CSS
+import './index.css';
+
+// Components
+import Home from './components/Home';
+
+ReactDOM.render(
+  	<Home />,
+  document.getElementById('root')
+);
